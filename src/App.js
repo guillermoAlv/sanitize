d72e5721c44d98e06615388a339c28f32e09aa16
Guillermo Alvarez <guillermoalvma@gmail.com>
2021-01-24 11:52:41 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+import RawEventPage from './components/RawEventPage'
 
 
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={()=>{fetch('https://vtn44sn818.execute-api.eu-west-1.amazonaws.com/Prod/sanitize-raw').then(response => response.json()).then(data => console.log(data));}}>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [content, setContent] = useState([])
+  if (content.length === 0){
+    fetch('https://vtn44sn818.execute-api.eu-west-1.amazonaws.com/Prod/sanitize-raw').then(response => response.json()).then(data => setContent(data));
+    console.log(content)
+  }
+  
+  return (<RawEventPage/>);
 }
 
 export default App;
