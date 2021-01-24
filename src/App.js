447b63d877c704fce666544081a2e7ec8228d246
Guillermo Alvarez <guillermoalvma@gmail.com>
2021-01-24 10:16:07 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p onClick={()=>{fetch('https://vtn44sn818.execute-api.eu-west-1.amazonaws.com/Prod/sanitize-raw').then(response => response.json()).then(data => console.log(data));}}>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
